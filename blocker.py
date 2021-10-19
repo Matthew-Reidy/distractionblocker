dab4a9b146c9ad09207a46b8283e993ea5e0babc
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-10-19 09:55:16 -0700

    Update blocker.py

--- blocker.py
+++ blocker.py
@@ -1,11 +1,14 @@
 import time
-
-
-
-def set_timer():
-  desired_time= input
-  
-  
-def parse_file():
- blacklist = open("blacklist.txt", "r") 
+import subprocess
+class DistractionBlocker:
+    def set_batch():
+        print("enter the urls of the sites you would like blocked")
+        commands = input()
+        blacklist = open('C:\Windows\System32\drivers\etc', 'w')
+        blacklist.write(commands)
+        blacklist.close()
+    def set_timer():    
+        time_durration= input
+    def run_batch(time_durration):
+        subprocess.call(['C:\Windows\System32\drivers\etc\blacklist.bat'])
 
