75ee92fe4b671d6d8e6f6db1eb2ff86edee41251
Matthew William Reidy <48036844+Matthew-Reidy@users.noreply.github.com>
2021-10-20 11:50:31 -0700

    Update blocker.py

--- blocker.py
+++ blocker.py
@@ -1,26 +1,33 @@
 #windows client
-
+import time 
 import subprocess
+
+
 class DistractionBlocker:
     def __init__(self, time_interval, commands):
         self.time_interval = time_interval
         self.commands=commands
     
     def set_batch(self): #creates batch file or appends new blocked sites to a batch file
-        print("enter the urls of the sites you would like blocked")
-        blacklist = open("C:\Windows\System32\drivers\etc", "a")
-        blacklist.write('''@echo off
+        blacklist = open("C:\Windows\System32\drivers\etc\blacklist.bat", "a")
+        blacklist.write ('''@echo off
                          set hostspath=%windir%\System32\drivers\etc\hosts\
                          echo 127.0.0.1''' + self.commands + '''>> %hostspath%
                          exit
                          '''
-                         )
-    def time_cleaner():
-        self.time_interval    
-    def run_batch(time_durration):
-        subprocess.call(['C:\Windows\System32\drivers\etc\blacklist.bat'])
+                        )
+        blacklist.close()
 
+    def time_cleaner(self, durration):
+        start = time.time()
+        end = self.time_interval + start
+        durration = min(self.time_interval / 60 )  
 
-process = DistractionBlocker(60, "www.facebook.com")
-print(process)
+    def run_batch(self, durration, start, blacklist):
+     subprocess.call(['C:\Windows\System32\drivers\etc\blacklist.bat'])
+     if start >= durration:
+        blacklist.kill()
+        
+
+DistractionBlocker(60, "www.facebook.com")
 
